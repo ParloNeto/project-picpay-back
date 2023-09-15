a985c25ba4d0e67aced4dd500eed7439821b19e2
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-09-14 21:16:47 -0300

    First Commit

---
+++ src/main/java/com/systempicpay/SystempicpayApplication.java
@@ -0,0 +1,13 @@
+package com.systempicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystempicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystempicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/systempicpay/SystempicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systempicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystempicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
