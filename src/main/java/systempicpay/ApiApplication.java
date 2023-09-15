6ad3dc21fbdaae516c6da5559d3da3edf3ea3f53
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-09-14 21:45:35 -0300

    configurated initial application.properties and pom.xml

--- src/main/java/systempicpay/ApiApplication.java
+++ src/main/java/systempicpay/ApiApplication.java
@@ -1,13 +1,13 @@
-package com.systempicpay;
+package systempicpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SystempicpayApplication {
+public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SystempicpayApplication.class, args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
 
 }
--- src/test/java/com/systempicpay/ApiApplicationTests.java
+++ src/test/java/com/systempicpay/ApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SystempicpayApplicationTests {
+class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
